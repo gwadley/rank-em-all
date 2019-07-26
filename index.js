14d11198589bd0e4478b2a0f341d5aac20c19dc9
Wadley, Gregory <gwadley@netsuite.com>
2019-07-26 13:58:52 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,101 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const db = require('./queries')
+const port = 3000
+var EloRating = require('elo-rating');
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.use(express.static('public'))
+
+
+app.get('/', function(req, res) {
+    res.sendFile('./index.html');
+});
+
+//Endpoint which returns the ranked table of pokemon
+app.get('/pokemonRanked',db.getPokemonRanked)
+
+//The UI calls this function to get 2 pokemon for the user to choose between. Because the calls to the DB are asyncronous, we first ask for data for a specific ID
+//Then when that is complete, ask for data for a pokemon within an acceptable ELO range (default 50)
+app.get('/getPokemonPair', function(req,res){
+	var returnData=[]
+	var r1 = Math.floor(Math.random() * 809)+1;
+	getAllPokemonData(r1).then(function(value){
+		returnData.push(value[0]);
+
+		getFairPokemonMatchup(value[0]["id"],value[0]["rating"],50).then(function(value){
+			returnData.push(value[0]);
+			res.send(returnData);
+		})
+	})
+})
+
+/*
+This function takes a POST body that looks like this:
+{
+	pokemon1:<id>,
+	pokemon2:<id>,
+	win
+}
+
+where win is a boolean value for whether pokemon1 was chosen. We do this through a boolean because that's what the ELO function expects.
+
+It then makes calls to the DB to update ratings of both pokemon
+*/
+app.post('/updateRankings', function(req,res){
+
+	var id1="";
+	var id2="";
+	var wins1;
+	var wins2;
+	var losses1;
+	var losses2;
+	var rating1;
+	var rating2;
+	var win = req.body["win"]
+	
+	getAllPokemonData(req.body["pokemon1"]).then(function(value){
+		id1=value[0]["id"]
+		rating1=value[0]["rating"]
+		wins1=value[0]["wins"]
+		losses1=value[0]["losses"]
+
+		getAllPokemonData(req.body["pokemon2"]).then(function(value){
+			id2=value[0]["id"]
+			rating2=value[0]["rating"]
+			wins2=value[0]["wins"]
+			losses2=value[0]["losses"]
+
+			var newRatings = ratingChanges(rating1,rating2,win)
+
+			if(win){
+				updatePokemonRanking(id1,newRatings[0],parseInt(wins1)+1,parseInt(losses1))
+				updatePokemonRanking(id2,newRatings[1],parseInt(wins2),parseInt(losses2)+1)
+			} else {
+				updatePokemonRanking(id1,newRatings[0],parseInt(wins1),parseInt(losses1)+1)
+				updatePokemonRanking(id2,newRatings[1],parseInt(wins2)+1,parseInt(losses2))
+			}
+		})
+		
+	})
+
+	res.send('Updated Rankings')
+})
+
+//This function takes 2 ratings and a winner, and uses the ELO funciton to calculate new ratints
+function ratingChanges(rating1,rating2,win){
+	result = EloRating.calculate(rating1,rating2,win)
+	return [result.playerRating,result.opponentRating]
+}
+
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
---
+++ queries.js
@@ -0,0 +1,65 @@
+const Pool = require('pg').Pool
+const jQuery = require('jQuery')
+const pool = new Pool({
+  user: 'me',
+  host: 'localhost',
+  database: 'api',
+  password: 'password',
+  port: 5432,
+})
+
+//This constant returns the entire table of pokemon ordered by their rating. It only returns name and ID because end users don't see the actual values
+const getPokemonRanked = (request, response) => {
+  pool.query('SELECT id,name,rating FROM pokemon ORDER BY rating DESC', (error,results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+    })
+}
+
+//this gets all the data from a single row corresponding to a pokemon ID
+getAllPokemonData = async function(id){
+  try{
+  var data = await pool.query('SELECT * FROM pokemon WHERE id = '+id);
+  } catch(error){
+    throw error
+  }
+  return data.rows;
+}
+
+//this function finds a pokemon who's ELO is within an acceptable range from the one it is given.
+//If no such pokemon can be found (a pokemon is too popular or unpopular) this function calls itself recursively with increasingly large ranges
+getFairPokemonMatchup = async function(id,rating,range){
+  var data = {}
+  var upperRating = parseInt(rating);
+  var lowerRating = parseInt(rating);
+  var returnData = {};
+  try{
+    data = await pool.query('SELECT * FROM pokemon WHERE rating BETWEEN '+(lowerRating-range)+' AND '+(upperRating+range)+' AND id != '+id+' ORDER BY RANDOM() LIMIT 1;')
+  } catch(error){
+    throw error
+  }
+
+  if(data.hasOwnProperty("rows")){
+    return data.rows;
+  } else{
+    return getFairPokemonMatchup(id,rating,range+50);
+  }
+}
+
+//function to update the rating of a pokemon.
+updatePokemonRanking = async  function(id,rating,wins,losses){
+  try{
+      var data = await pool.query('UPDATE pokemon SET rating = '+rating+',wins = '+wins+', losses = '+losses+' WHERE id = '+id)
+  } catch(error){
+    throw error
+  }
+}
+
+module.exports = {
+  getPokemonRanked,
+  getAllPokemonData,
+  updatePokemonRanking,
+  getFairPokemonMatchup
+}
